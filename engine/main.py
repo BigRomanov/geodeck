bb057164c03da8f2712068b0e2caddc82060a570
BigRomanov <big.romanov@gmail.com>
2015-12-26 18:21:00 +0200

    Moving on

--- engine/main.py
+++ engine/main.py
@@ -1,26 +1,50 @@
+######
+# Python 3 version
+# Main python file for tokenizing and analyzing tokes retrieved from a file supplied as a parameter
+
 import sys
+import nltk
+import enchant
 
+from nltk.tokenize import sent_tokenize
 
-def readContentFromFile(filename):
-  with open(filename, encoding="utf8") as f:
-    content = f.readlines()
+# Utility functions
+def readTextFromFile(filename):
+  with open(filename, "r", encoding="utf-8") as f:
+    content = f.read()
     return content
-  
 
+  
+# MAIN
 if __name__ == "__main__":
 
   if len(sys.argv) != 2:
     print ("Invalid parameters")
     exit(1)
 
-
+  # Initialization
   filename = sys.argv[1]
   print ("Engine running on file %s" % filename)
 
-  content = readContentFromFile(filename)
+  # Read the text
+  text = readTextFromFile(filename)
 
-  for line in content:
-    print(line)
+  # Split by sentences
+  #sentences = sent_tokenize(text)
+  #print(sentences)
 
+  # Tokens
+  d = enchant.Dict("en_US")
+
+  # Take only tokens which start with capital letter and are not in the english dictionary
+  tokens = [tok.strip().strip(":") for tok in text.split() if tok[0].isupper() and not d.check(tok)]
+  #print(tokens[:100])
+
+  # Measure token frequency
+  Freq_dist_nltk=nltk.FreqDist(tokens)
+  for k,v in Freq_dist_nltk.items():
+    print(str(k)+':'+str(v))
+
+  
 
   
