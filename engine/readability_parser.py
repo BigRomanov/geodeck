f106835ed7e810a8b2a5cacca9405b4bda83ffee
BigRomanov <big.romanov@gmail.com>
2016-01-02 18:26:43 +0200

    Readability

--- engine/readability_parser.py
+++ engine/readability_parser.py
@@ -1,9 +1,10 @@
 # Readability API Wrapper
-import readability
+
 from readability import ParserClient
+from bson.json_util import dumps
 import utils
 
-PARSER_TOKEN="69277572f6b7c2842f599c02912ab989f3693eae"
+PARSER_TOKEN=""
 
 # TEST MAIN
 if __name__ == "__main__":
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
   source = "http://skift.com/2015/12/28/landmark-new-york-city-hotels-pledge-to-cut-greenhouse-gas-emissions/"
 
-  parser_client = ParserClient(PARSER_TOKEN)
+  parser_client = ParserClient(token=PARSER_TOKEN)
   parser_response = parser_client.get_article(source)
   article = parser_response.json()
 
